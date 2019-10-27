a28ec124aefe2442d796496b2c2db8b4f61bc9b4
kath <iamkate4@gmail.com>
2019-10-27 22:32:48 +0200

    set buttons in modal

--- script.js
+++ script.js
@@ -53,8 +53,8 @@ var menuList = document.getElementsByClassName('menu');
 
   const cartArray = [];
 
+  const cart = document.getElementsByClassName('cart__link');
   function initCart(){
-    const cart = document.getElementsByClassName('cart__link')[0];
     
     var products = document.getElementsByClassName('product');
     var productBtns =[];
@@ -63,12 +63,12 @@ var menuList = document.getElementsByClassName('menu');
       var productBtn = document.getElementsByClassName('product__btn')[product];
 
       if(product < products.length) {
-        productBtns.push(productBtn);
 
         productBtn.addEventListener('click', function addToCart(ev){
           ev.preventDefault();
           const priceText = products[product].getElementsByClassName('product__price')[0].textContent;
           const nameText = products[product].getElementsByClassName('product__title')[0].textContent;
+          const img = products[product].getElementsByClassName("product__img")[0].src;
 
           var price = parseFloat(getPrice(priceText));
           var name = getName(nameText);
@@ -82,9 +82,9 @@ var menuList = document.getElementsByClassName('menu');
           });
         
           if(isPresent == false)
-            cartArray.push(makeCartEl(products[product].id, price, name))
+            cartArray.push(makeCartEl(products[product].id, price, name, img))
       
-          refreshCart(cart);
+          refreshCart(cart[0]);
         });
 
       }
@@ -92,13 +92,14 @@ var menuList = document.getElementsByClassName('menu');
 
   }
 
-  function makeCartEl(id, price, name){
+  function makeCartEl(id, price, name, img){
     return {
       id: id,
       price: price,
       name: name,
       amount: 1,
-      currency: "$"
+      currency: "$",
+      img: img
     }
   }
 
@@ -191,28 +192,79 @@ function refreshModal(total){
 
   var modalTotal = document.getElementsByClassName("modal__footer")[0];
   
-  if(cartArray.length == 0) modalContent.innerHTML = `Cart is empty`;
-  else{
+  if(cartArray.length == 0) {
+    modalContent.innerHTML = `Cart is empty`;
+    modalTotal.innerHTML = `<h3>Total: ${0}<span class = "currency">$</span></h3>`
+  } else{
     modalContent.innerHTML = "";
     cartArray.forEach(element => {
-      modalContent.innerHTML += createProductHTML(element.name, element.price, element.amount, element.currency);
+      modalContent.innerHTML += createProductHTML(element.name, element.price, element.amount, element.currency, element.img);
     });
 
   modalTotal.innerHTML = `<h3>Total: ${total.toFixed(2)}<span class = "currency">$</span></h3>`
   }
+
+  setModalCloseBtns();
+  setModalAmountBtns();
 }
 
-function createProductHTML(name, price, amount, currency){
+function createProductHTML(name, price, amount, currency,img){
   return `<div class = "cartItem">
+    <div class = "cartItem__img--wrapper">
+      <img src="${img}" class = "cartItem__img">
+    </div>
     <a class = "cartItem__name" href="#" >${name}</a>
     <div class = "cartItem__amount-bar">
-      <button class = "cartItem__btn-m">-</button>
+      <button class = "cartItem__btn-m cartItem__btn" >-</button>
       ${amount}
-      <button class = "cartItem__btn-pl">+</button>
+      <button class = "cartItem__btn-pl cartItem__btn">+</button>
     </div>
     <span class="cartItem__price">
       ${(price * amount).toFixed(2)} <span class = "currency">${currency}</span>
     </span>
     <button class="cartItem__close">&times;</button>
   </div>`;
+}
+
+function setModalCloseBtns(){
+  var cartItems = document.getElementsByClassName('cartItem');
+
+  for(let cartItem in cartItems){
+    var btn = document.getElementsByClassName("cartItem__close")[cartItem];
+
+    if(cartItem < cartItems.length){
+      btn.addEventListener('click', ev => {
+        ev.preventDefault();
+        cartArray.splice(cartItem, 1);
+        refreshCart(cart[0]);
+      });
+    }
+
+  }
+}
+
+function setModalAmountBtns(){
+  var cartItems = document.getElementsByClassName('cartItem');
+  
+  for(let cartItem in cartItems){
+    var btnMinus = document.getElementsByClassName("cartItem__btn-m")[cartItem];
+    var btnPlus = document.getElementsByClassName("cartItem__btn-pl")[cartItem];
+
+    if(cartItem < cartItems.length){
+      btnMinus.addEventListener('click', ev => {
+        ev.preventDefault();
+
+        if(cartArray[cartItem].amount == 1) cartArray.splice(cartItem, 1);
+        else cartArray[cartItem].amount--;
+        refreshCart(cart[0]);
+      });
+
+      btnPlus.addEventListener('click', ev =>{
+        ev.preventDefault();
+        cartArray[cartItem].amount++;
+        refreshCart(cart[0]);
+      });
+    }
+
+  }
 }
